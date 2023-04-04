a1cfc6a2ca716d007e6cc701d74e6b545f10ae4d
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-04-04 01:55:52 +0000

    lesson9.1

---
+++ files/new.rb
@@ -0,0 +1,35 @@
+#インスタンスメソッド
+class Car
+
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+car = Car.new
+car.run(5)
+car.turn("右")
+car.move("右",6)
+
+#クラスメソッド
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
+Car.turn("右")
